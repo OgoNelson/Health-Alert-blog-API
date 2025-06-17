ccfba09a2ee7316283c49d318db3ac62a6221f4d
Ogo Nelson <goldnelsgroup@gmail.com>
2025-06-18 00:24:13 +0100

    connected the server to a database

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require("./main");
-// const database = require("./config/database");
+const database = require("./config/database");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
-// database.connectDB();
+database.connectDB();
 
 const port = process.env.PORT || 3000;
 

dc897f5c5b0a6860cfc2a6ece4ca45b3d25d2dfc
Ogo Nelson <goldnelsgroup@gmail.com>
2025-06-17 23:57:27 +0100

    initial setup of express server

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require("express");
+// const tasksRouter = require("./tasks/tasks.router");
+// const usersRouter = require("./users/users.router");
+
+const app = express();
+
+app.use(express.json()); // parse json body
+
+app.get("/", (req, res) => {
+  res.send("Health Alert blog API");
+});
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+// app.use("/api/v1/users", usersRouter);
+// app.use("/api/v1/tasks", tasksRouter);
+
+module.exports = app;
